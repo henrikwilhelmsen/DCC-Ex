3ce11824632736469757aba2456d6f1ec8899b77
Henrik Wilhelmsen <henrik@hwanimation.tech>
2026-08-30 00:21:11 +0200

    add cli stubs, update lockfile

---
+++ src/dccex/cli.py
@@ -0,0 +1,27 @@
+# Copyright (C) 2026 Henrik Wilhelmsen.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at <https://mozilla.org/MPL/2.0/>.
+
+"""dccex.cli - A CLI for finding and running DCC executables."""
+
+
+def maya() -> None:
+    """Launch Maya."""
+
+
+def mayapy() -> None:
+    """Launch mayapy."""
+
+
+def mobu() -> None:
+    """Launch MotionBuilder."""
+
+
+def mobupy() -> None:
+    """Launch mobupy."""
+
+
+def blender() -> None:
+    """Launch Blender."""
